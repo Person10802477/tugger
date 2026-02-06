952d50756e51e715c066b431e140c6773c15696c
Person10802477 <59040987+Person10802477@users.noreply.github.com>
2026-02-05 17:39:52 -0700

    Update tugger-ratio.user.js

--- tugger-ratio.user.js
+++ tugger-ratio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tugger Ratio FCLM
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Inserts a Tugger Ratio line in FCLM below STPS using the following formula: (ETI hours + ESTP hours) / (Tugger hours + Trans hours)
 // @updateURL    https://raw.githubusercontent.com/Person10802477/tugger/main/tugger-ratio.user.js
 // @downloadURL  https://raw.githubusercontent.com/Person10802477/tugger/main/tugger-ratio.user.js
